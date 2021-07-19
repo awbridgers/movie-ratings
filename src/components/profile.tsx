0a612118e77ec611a419edbbd3cd356ab9a13ecb
Adam Bridgers <awbridgers@users.noreply.github.com>
2021-07-18 22:41:12 -0400

    adds the ability to change email, display name, and password to the profile page

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,29 +1,41 @@
-import React, { useContext } from 'react'
-import {AuthContext} from '../firebase/authProvider'
-import '../styles/profile.css'
-
-
+import React, {useContext, useState} from 'react';
+import {AuthContext} from '../firebase/authProvider';
+import '../styles/profile.css';
+import ChangeProfileInfo from './changeProfileInfo';
 
 const Profile = () => {
-  const user = useContext(AuthContext)
-  return(
-    <div className = 'profile'>
-      <div className = 'settings'>
-        <div className = 'settingsTitle'>
-          Account Settings
-        </div>
-        <div className = 'settingsBody'>
-          <div className = 'settingsInfo'>
-            <table className = 'infoTable'>
+  const [showChange, setShowChange] = useState<'Password'|'Name'|'Email' | null>(null);
+  const user = useContext(AuthContext)!;
+  return (
+    <div className="profile">
+      {showChange && (
+        <ChangeProfileInfo
+          type={showChange}
+          currentEmail={user.email!}
+          currentName={user.displayName!}
+          back = {()=>setShowChange(null)}
+        />
+      )}
+      <div className="settings">
+        <div className="settingsTitle">Account Settings</div>
+        <div className="settingsBody">
+          <div className="settingsInfo">
+            <table className="infoTable">
               <tbody>
                 <tr>
-                  <td>Email:</td><td>{user?.email}</td><td>Change</td>
+                  <td>Email</td>
+                  <td>{user.email}</td>
+                  <td className = 'change' onClick = {()=>setShowChange('Email')}>Change</td>
                 </tr>
                 <tr>
-                  <td>Display Name:</td><td>{user?.displayName}</td><td>Change</td>
+                  <td>Display Name</td>
+                  <td>{user.displayName}</td>
+                  <td className = 'change' onClick = {()=>setShowChange('Name')}>Change</td>
                 </tr>
                 <tr>
-                  <td>Password:</td><td></td><td>Change</td>
+                  <td>Password</td>
+                  <td>N/A</td>
+                  <td className = 'change' onClick = {()=>setShowChange('Password')}>Change</td>
                 </tr>
               </tbody>
             </table>
@@ -31,7 +43,7 @@ const Profile = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
