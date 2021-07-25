69122af6d37c760b57aef5bce9fc17abae1dfccd
Adam Bridgers <awbridgers@users.noreply.github.com>
2021-07-25 11:37:02 -0400

    finished the profile page with the ability to delete the user account

--- src/components/changeProfileInfo.tsx
+++ src/components/changeProfileInfo.tsx
@@ -5,9 +5,10 @@ import '../styles/profile.css';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import {AuthContext} from '../firebase/authProvider';
-
+import {db} from '../firebase/config';
+import {FirebaseContext} from '../firebase/provider';
 interface Props {
-  type: 'Password' | 'Email' | 'Name';
+  type: 'Password' | 'Email' | 'Name' | 'Delete';
   currentEmail: string;
   currentName: string;
   back: () => void;
@@ -27,7 +28,9 @@ const ChangeProfileInfo = ({type, back, currentEmail, currentName}: Props) => {
   const [currentPassword, setCurrentPassword] = useState<string>('');
   const [email, setEmail] = useState<string>('');
   const [name, setName] = useState<string>('');
-  const user = useContext(AuthContext);
+  const [deleteText, setDeleteText] = useState<string>('');
+  const user = useContext(AuthContext)!; //user must exist to access this page
+  const userMovies = useContext(FirebaseContext).userMovie;
   const [errors, setErrors] = useState<Errors>({
     displayName: '',
     confirmPassword: '',
@@ -35,8 +38,8 @@ const ChangeProfileInfo = ({type, back, currentEmail, currentName}: Props) => {
     password: '',
     currentPassword: '',
   });
-  const handleSubmit = () => {
-
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
     setErrors({
       displayName: '',
       confirmPassword: '',
@@ -44,38 +47,63 @@ const ChangeProfileInfo = ({type, back, currentEmail, currentName}: Props) => {
       password: '',
       currentPassword: '',
     });
+    //reauthenticate the user
+    const cred = firebase.auth.EmailAuthProvider.credential(
+      currentEmail,
+      currentPassword
+    );
     switch (type) {
       case 'Email':
-        //reauthenticate the user
-        const cred = firebase.auth.EmailAuthProvider.credential(
-          currentEmail,
-          currentPassword
-        );
-        user!
+        user
           .reauthenticateWithCredential(cred)
           .then((userCredential) => {
-            userCredential.user?.updateEmail(email).then(()=>{
-              setSuccess(true)
-            }).catch(e=>{
-              setErrors(prev=>({...prev, email: e.message}))
-            })
-          }).catch(e=>{
-            setErrors(prev=>({...prev, currentPassword: e.message}))
-          });
-          break;
-      case 'Name':
-        if(name.length < 3){
-          setErrors(prev=>({...prev, displayName: 'Name must be at least 3 characters.'}))
-        }
-        else if (!/^[a-zA-Z]+$/g.test(name)){
-          setErrors(prev=>({...prev, displayName: 'Name can only contain letters.'}))
-        }
-        else{
-          user?.updateProfile({displayName: name}).then(()=>{
-            setSuccess(true)
-          }).catch((e)=>{
-            setErrors(prev=>({...prev, displayName: e.message}))
+            userCredential.user
+              ?.updateEmail(email)
+              .then(() => {
+                setSuccess(true);
+              })
+              .catch((e) => {
+                setErrors((prev) => ({...prev, email: e.message}));
+              });
           })
+          .catch((e) => {
+            setErrors((prev) => ({...prev, currentPassword: e.message}));
+          });
+        break;
+      case 'Name':
+        if (name.length < 3) {
+          setErrors((prev) => ({
+            ...prev,
+            displayName: 'Name must be at least 3 characters.',
+          }));
+        } else if (!/^[a-zA-Z]+$/g.test(name)) {
+          setErrors((prev) => ({
+            ...prev,
+            displayName: 'Name can only contain letters.',
+          }));
+        } else {
+          //need to update display name in users and at each movie where it appears
+          let updates: {[key: string]: any} = {};
+          updates[`users/${user.uid}/displayName`] = name;
+          userMovies.forEach((movie) => {
+            updates[`movies/${movie.name}/ratings/${user.uid}/displayName`] =
+              name;
+          });
+          db.ref()
+            .update(updates)
+            .then(() => {
+              user
+                .updateProfile({displayName: name})
+                .then(() => {
+                  setSuccess(true);
+                })
+                .catch((e) => {
+                  setErrors((prev) => ({...prev, displayName: e.message}));
+                });
+            })
+            .catch((e) =>
+              setErrors((prev) => ({...prev, displayName: e.message}))
+            );
         }
         break;
       case 'Password':
@@ -86,11 +114,7 @@ const ChangeProfileInfo = ({type, back, currentEmail, currentName}: Props) => {
           }));
         } else {
           //then reauth user
-          const cred = firebase.auth.EmailAuthProvider.credential(
-            currentEmail,
-            currentPassword
-          );
-          user!
+          user
             .reauthenticateWithCredential(cred)
             .then((userCredential) => {
               //if user authorized, change password
@@ -107,6 +131,32 @@ const ChangeProfileInfo = ({type, back, currentEmail, currentName}: Props) => {
               setErrors((prev) => ({...prev, currentPassword: e.message}));
             });
         }
+        break;
+      case 'Delete':
+        //delete all the users ratings
+
+        user
+          .reauthenticateWithCredential(cred)
+          .then((userCredential) => {
+            if (userCredential.user) {
+              const reCred = userCredential.user;
+              const deleteDB: {[key: string]: any} = {};
+              userMovies.forEach((movie) => {
+                deleteDB[`movies/${movie.name}/ratings/${reCred.uid}`] = null;
+              });
+              deleteDB[`users/${reCred.uid}`] = null;
+              db.ref()
+                .update(deleteDB)
+                .then(() => {
+                  reCred.delete().then(() => {
+                    setSuccess(true);
+                  });
+                });
+            }
+          })
+          .catch((e) => {
+            setErrors((prev) => ({...prev, currentPassword: e.message}));
+          });
         break;
     }
   };
@@ -127,36 +177,37 @@ const ChangeProfileInfo = ({type, back, currentEmail, currentName}: Props) => {
     <div className="logIn">
       <div className="logInBox">
         <div className="logInTitle">
-          <h2>Change {type}</h2>
+          {type != 'Delete' && <h2>Change {type}</h2>}
+          {type === 'Delete' && <h2>Delete Account</h2>}
         </div>
         <Form className="form">
           {type === 'Email' && (
             <>
-            <Form.Group as={Row} controlId="formBasicEmail">
-              <Form.Label column>Current Email:</Form.Label>
-              <Col>
+              <Form.Group as={Row} controlId="formBasicEmail">
+                <Form.Label column>Current Email:</Form.Label>
+                <Col>
+                  <Form.Control
+                    className="plainText"
+                    plaintext
+                    readOnly
+                    defaultValue={currentEmail}
+                  ></Form.Control>
+                </Col>
+              </Form.Group>
+              <Form.Group controlId="newEmailForm">
+                <Form.Label>New Email</Form.Label>
                 <Form.Control
-                  className="plainText"
-                  plaintext
-                  readOnly
-                  defaultValue={currentEmail}
-                ></Form.Control>
-              </Col>
-            </Form.Group>
-            <Form.Group controlId="newEmailForm">
-              <Form.Label>New Email</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter New Email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-                isInvalid={errors.email !== ''}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.email}
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group controlId="formBasicCurrentPassword">
+                  type="email"
+                  placeholder="Enter New Email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                  isInvalid={errors.email !== ''}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.email}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="formBasicCurrentPassword">
                 <Form.Label>Current Password</Form.Label>
                 <Form.Control
                   type="password"
@@ -169,7 +220,7 @@ const ChangeProfileInfo = ({type, back, currentEmail, currentName}: Props) => {
                   {errors.currentPassword}
                 </Form.Control.Feedback>
               </Form.Group>
-          </>
+            </>
           )}
           {type === 'Name' && (
             <>
@@ -246,15 +297,56 @@ const ChangeProfileInfo = ({type, back, currentEmail, currentName}: Props) => {
               </Form.Group>
             </>
           )}
+          {type === 'Delete' && (
+            <>
+              <Form.Text className="warning">
+                Warning: This action will permanently delete your account!
+              </Form.Text>
+              <Form.Group controlId="formBasicCurrentPassword">
+                <Form.Label>Current Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Current Password"
+                  value={currentPassword}
+                  onChange={(e) => setCurrentPassword(e.target.value)}
+                  isInvalid={errors.currentPassword !== ''}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.currentPassword}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="delete">
+                <Form.Label>Enter 'DELETE' to delete your account.</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="enter DELETE"
+                  onChange={(e) => setDeleteText(e.target.value)}
+                  value={deleteText}
+                />
+              </Form.Group>
+            </>
+          )}
           <div className="logInButtons">
-            <Button
-              variant="primary"
-              type="submit"
-              id="submitButton"
-              onClick={handleSubmit}
-            >
-              Submit
-            </Button>
+            {type !== 'Delete' && (
+              <Button
+                variant="primary"
+                type="submit"
+                id="submitButton"
+                onClick={handleSubmit}
+              >
+                Submit
+              </Button>
+            )}
+            {type === 'Delete' && (
+              <Button
+                variant="warning"
+                id="deleteButton"
+                onClick={handleSubmit}
+                disabled={deleteText !== 'DELETE'}
+              >
+                Delete Account
+              </Button>
+            )}
             <Button variant="danger" id="backButton" onClick={back}>
               Back
             </Button>
--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,21 +1,28 @@
 import React, {useContext, useState} from 'react';
-import { useMediaQuery } from 'react-responsive';
+import {useMediaQuery} from 'react-responsive';
 import StarRatings from 'react-star-ratings';
 import {AuthContext} from '../firebase/authProvider';
-import { FirebaseContext } from '../firebase/provider';
+import {FirebaseContext} from '../firebase/provider';
 import '../styles/profile.css';
-import { averageRating } from '../util/averageRating';
+import {averageRating} from '../util/averageRating';
 import ChangeProfileInfo from './changeProfileInfo';
 import RatingTable from './ratingTable';
-import '../styles/viewerCard.css'
+import '../styles/viewerCard.css';
+import {Button} from 'react-bootstrap';
 
 const Profile = () => {
-  const [showChange, setShowChange] = useState<'Password'|'Name'|'Email' | null>(null);
+  const [showChange, setShowChange] = useState<
+    'Password' | 'Name' | 'Email' | 'Delete' | null
+  >(null);
   const user = useContext(AuthContext)!;
-  const isMobile = useMediaQuery({maxWidth: 700})
+  const isMobile = useMediaQuery({maxWidth: 700});
   const userMovies = useContext(FirebaseContext).userMovie;
-  const highestRated = userMovies.reduce((accumulator, current)=>accumulator.score > current.score ? accumulator : current);
-  const lowestRated = userMovies.reduce((accumulator, current)=>accumulator.score < current.score ? accumulator : current);
+  const highestRated = userMovies.length? userMovies.reduce((accumulator, current) =>
+    accumulator.score > current.score ? accumulator : current
+  ) : null
+  const lowestRated = userMovies.length ? userMovies.reduce((accumulator, current) =>
+    accumulator.score < current.score ? accumulator : current
+  ):null;
 
   return (
     <div className="profile">
@@ -24,7 +31,7 @@ const Profile = () => {
           type={showChange}
           currentEmail={user.email!}
           currentName={user.displayName!}
-          back = {()=>setShowChange(null)}
+          back={() => setShowChange(null)}
         />
       )}
       <div className="settings">
@@ -36,29 +43,45 @@ const Profile = () => {
                 <tr>
                   <td>Email</td>
                   <td>{user.email}</td>
-                  <td className = 'change' onClick = {()=>setShowChange('Email')}>Change</td>
+                  <td className="change" onClick={() => setShowChange('Email')}>
+                    Change
+                  </td>
                 </tr>
                 <tr>
                   <td>Display Name</td>
                   <td>{user.displayName}</td>
-                  <td className = 'change' onClick = {()=>setShowChange('Name')}>Change</td>
+                  <td className="change" onClick={() => setShowChange('Name')}>
+                    Change
+                  </td>
                 </tr>
                 <tr>
                   <td>Password</td>
                   <td>N/A</td>
-                  <td className = 'change' onClick = {()=>setShowChange('Password')}>Change</td>
+                  <td
+                    className="change"
+                    onClick={() => setShowChange('Password')}
+                  >
+                    Change
+                  </td>
                 </tr>
               </tbody>
             </table>
           </div>
         </div>
       </div>
-      <div className = 'settings'>
-        <div className = 'settingsTitle'>My Ratings</div>
-        <div className = 'setttingsBody'>
-       
-          <RatingTable movie ratings={userMovies} isMobile = {isMobile} title = ''/>
+      <div className="settings">
+        <div className="settingsTitle">My Ratings</div>
+        <div className="setttingsBody">
+          <RatingTable
+            movie
+            ratings={userMovies}
+            isMobile={isMobile}
+            title=""
+          />
         </div>
+      </div>
+      <div className="delete">
+        <Button variant="danger" onClick = {()=>setShowChange('Delete')}>Delete Account</Button>
       </div>
     </div>
   );
